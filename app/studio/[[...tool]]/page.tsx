ad43461596b84f89334ef5bd82b5f4436bacb03d
pronurturesphereltd-wq <pronurturesphereltd@gmail.com>
2026-06-14 08:53:15 -0700

    Remove embedded studio - redirect to Sanity cloud

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,7 +1,13 @@
 'use client'
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
+import { useEffect } from 'react'
 
 export default function Studio() {
-  return <NextStudio config={config} />
+  useEffect(() => {
+    window.location.href = `https://pronurturesphereltd-wq.sanity.studio`
+  }, [])
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', fontFamily: 'sans-serif' }}>
+      <p>Redirecting to Sanity Studio...</p>
+    </div>
+  )
 }
