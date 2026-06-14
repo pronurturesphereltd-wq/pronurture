a69cb726d5fc6f8001a9e59e20e8e81eaf46556c
pronurturesphereltd-wq <pronurturesphereltd@gmail.com>
2026-06-14 09:10:52 -0700

    Hardcode Sanity fallback values

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -2,8 +2,8 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'cfu3qevi',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01',
   useCdn: true,
 })
