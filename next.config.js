409b55b7593fe792bb1f4d83beed3bfb5c508157
pronurturesphereltd-wq <pronurturesphereltd@gmail.com>
2026-06-14 08:45:46 -0700

    Fix Sanity Studio build error

--- next.config.js
+++ next.config.js
@@ -6,5 +6,14 @@ const nextConfig = {
     ],
   },
   transpilePackages: ['sanity', 'next-sanity'],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      }
+    }
+    return config
+  },
 }
 module.exports = nextConfig
